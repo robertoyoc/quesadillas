9caaa3520b333eadc12180bf3bae2c89dd120068
robertoyoc <a01172599@itesm.mx>
2017-10-08 17:11:01 -0500

    añadido models sucursal y turno

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+
+    let sucursales = this.store.findAll('sucursal');
+    let turnos = this.store.findAll('turno');
+
+    return {
+      sucursal: sucursales,
+      turno: turnos
+    }
+  }
 });
