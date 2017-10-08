cb0467051c99bd9e64137a6d60e6439afdafe203
robertoyoc <a01172599@itesm.mx>
2017-10-08 17:19:43 -0500

    terminado login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -12,65 +12,57 @@ export default Ember.Controller.extend({
       newTurno.save();
     },
     init(user, pass){
-      console.log(this.get('session'));
 
       let Controller = this;
-      console.log(Controller.get('session'))
+      swal({
+        title: 'Cargando...',
+        onOpen: function (){
+          swal.showLoading();
+          Controller.get('session').open('firebase', {
+              provider: 'password',
+              email: user,
+              password: pass
+          }).then(function(){
+            swal.close();
+            //registrado correctamente
+            Controller.transitionToRoute('index');
 
-      this.get('session').open('firebase', {
-        provider: 'password',
-        email: 'test@example.com',
-        password: 'password1234'
+          }, function(data){
+            console.log(data)
+            swal.close();
+            if(data.code=="auth/user-not-found"){ //no registrado
+              swal(
+                'Oops...',
+                'Parece que no estás registrado en nuestra web',
+                'error'
+              );
+            }
+            else if(data.code=="auth/wrong-password"){ //contraseña incorrecta
+              swal(
+                'Oops...',
+                'Contraseña incorrecta!',
+                'error'
+              );
+            }
+            else if(data.code=="auth/invalid-email"){
+              swal(
+                'Oops...',
+                'Correo no válido',
+                'error'
+              );
+            }
+            else{ //no se mandaron datos
+              swal(
+                'Oops...',
+                'No has enviado ningún dato!',
+                'error'
+              );
+            }
+          });
+
+
+        }
       });
-      // swal({
-      //   title: 'Cargando...',
-      //   onOpen: function (){
-      //     swal.showLoading();
-      //     Controller.get('session').open('firebase', {
-      //         provider: 'password',
-      //         email: user,
-      //         password: pass
-      //     }).then(function(){
-      //       swal.close();
-      //       //registrado correctamente
-      //       Controller.transitionToRoute('index');
-
-      //     }, function(data){
-      //       console.log(data)
-      //       swal.close();
-      //       if(data.code=="auth/user-not-found"){ //no registrado
-      //         swal(
-      //           'Oops...',
-      //           'Parece que no estás registrado en nuestra web',
-      //           'error'
-      //         );
-      //       }
-      //       else if(data.code=="auth/wrong-password"){ //contraseña incorrecta
-      //         swal(
-      //           'Oops...',
-      //           'Contraseña incorrecta!',
-      //           'error'
-      //         );
-      //       }
-      //       else if(data.code=="auth/invalid-email"){
-      //         swal(
-      //           'Oops...',
-      //           'Correo no válido',
-      //           'error'
-      //         );
-      //       }
-      //       else{ //no se mandaron datos
-      //         swal(
-      //           'Oops...',
-      //           'No has enviado ningún dato!',
-      //           'error'
-      //         );
-      //       }
-      //     });
-
-
-      //   }
-      // });
 
     }
 
